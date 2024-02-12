469ac6d5148d258535476c78ffaad39f0c6ad153
navneetgehlotsst <148967647+navneetgehlotsst@users.noreply.github.com>
2024-02-12 12:53:07 +0530

    changes

--- app/Http/Controllers/Website/HomeController.php
+++ app/Http/Controllers/Website/HomeController.php
@@ -300,6 +300,7 @@ class HomeController extends Controller
         if(empty($userCheck)){
             return redirect()->back()->with('error', 'Invalid email..!');
         }else{
+            DB::table('password_reset_tokens')->where('email',$request->email)->delete();
             $token = Str::random(64);
             DB::table('password_reset_tokens')->insert(['email' => $request->email, 'token' => $token, 'created_at' => Carbon::now() ]);
 

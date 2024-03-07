4cb0548ec19474a3586d3f58c2923aec575c2486
navneetgehlotsst <148967647+navneetgehlotsst@users.noreply.github.com>
2024-03-07 17:05:07 +0530

    website changes

--- resources/views/web/publisher-play-video.blade.php
+++ resources/views/web/publisher-play-video.blade.php
@@ -196,25 +196,7 @@
 </div>
 <!-- All Video Carousel End-->
 
-<!-- Start Call To Action Area -->
-<div class="call-to-action-area py-5">
-    <div class="container">
-        <div class="row py-5">
-            <div class="col-md-12">
-                <div class="action-content text-center pb-5">
-                    <h2 class="text-dark fw-bold lh-base">Don't miss out on the latest updates, promotions, and exclusive content. Sign up for our newsletters by entering your email address below.</h2>
-                </div>
-            </div>
-            <div class="col-md-6 m-auto">
-                <div class="subscribe-section">
-                    <input type="email" placeholder="Enter your email">
-                    <button class="px-2 px-lg-5">Subscribe</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Call To Action Area -->
+@include('web.layouts.elements.newsletter')
 
 
 @endsection
